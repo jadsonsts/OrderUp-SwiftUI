1f77c9f039aaccd46d1fdd735af63bae9521095c
jadsonsts <jadson.s.carneiro@gmail.com>
2022-11-16 22:59:11 +1300

    first commit

---
+++ OrderUp/OrderUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderUpApp.swift
+//  OrderUp
+//
+//  Created by Jadson on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct OrderUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
